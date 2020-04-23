52599259c5f7fc2d56debe504c8617c1bbd22da7
JPedraza <printepolis@gmail.com>
2020-04-23 00:46:35 -0500

    Integracion de Librerias

--- _components.php
+++ _components.php
@@ -16,8 +16,214 @@ function Test(){
     echo "<label>Dato 2: <input type='number'></label>";
     echo "<label>Dato 3: <input type='date'></label>";
     echo "<label style=''>Dato 4: <input type='mail'></label>";
-
+    echo "<label style='background-color:transparent;'><input class='btn btn-Primary' type='submit' value='Enviar'></label>";
     echo "</form>";
 
+    toast("Ejemplo de Notificacion Toas",1,"");
+
 }
+
+
+
+
+function Toast($Texto,$Tipo,$img){
+    switch ($Tipo) {
+        case 0:
+            echo "<script>";
+                echo "$.toast('".$Texto."');   ";
+            echo "</script>";
+            break;
+        case 1: //Informativo
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: 'Information',
+                text: '".$Texto."',
+                showHideTransition: 'slide',
+                icon: 'info'
+            })
+            ";
+            echo "</script>";
+            break;
+       
+        case 2: //Error
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: 'Error',
+                text: '".$Texto."',
+                showHideTransition: 'slide',
+                icon: 'error'
+            })
+            ";
+            echo "</script>";
+            break;
+
+        case 3: //Warning
+                echo "<script>";
+                echo "
+                $.toast({
+                    heading: 'Warning',
+                    text: '".$Texto."',
+                    showHideTransition: 'slide',
+                    icon: 'warning'
+                })
+                ";
+                echo "</script>";
+                break;
+
+                
+
+        case 4: //Success
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: 'Success',
+                text: '".$Texto."',
+                showHideTransition: 'slide',
+                icon: 'success'
+            })
+            ";
+            echo "</script>";
+            break;
+    
+
+        case 5: //fijo
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: '',
+                text: '".$Texto."',                
+                hideAfter: false
+                
+            })
+            ";
+            echo "</script>";
+            break;
+        
+        case 6: //imagen normal
+                echo "<script>";
+                echo "
+                $.toast({
+                    heading: '',
+                    text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+                    hideAfter: false
+                    
+                })
+                ";
+                echo "</script>";
+        break;                
+
+
+        case 7: //imagen sucess
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: '',
+                text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+                hideAfter: false,
+                icon:'success'
+                
+            })
+            ";
+            echo "</script>";
+        break;                
+
+
+        case 8: //imagen warning
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: '',
+                text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+                hideAfter: false,
+                icon:'warning'
+                
+            })
+            ";
+            echo "</script>";
+        break;                
+
+        case 9: //imagen error
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: '',
+                text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+                hideAfter: false,
+                icon:'error'
+                
+            })
+            ";
+            echo "</script>";
+        break;                
+
+        case 10: //imagen normal auto
+            echo "<script>";
+            echo "
+            $.toast({
+                heading: '',
+                text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+                showHideTransition: 'slide'
+                
+            })
+            ";
+            echo "</script>";
+    break;                
+
+
+    case 11: //imagen sucess auto
+        echo "<script>";
+        echo "
+        $.toast({
+            heading: '',
+            text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+            
+            icon:'success',
+            showHideTransition: 'slide'
+            
+        })
+        ";
+        echo "</script>";
+    break;                
+
+
+    case 12: //imagen warning auto
+        echo "<script>";
+        echo "
+        $.toast({
+            heading: '',
+            text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+           
+            icon:'warning',
+            showHideTransition: 'slide'
+            
+        })
+        ";
+        echo "</script>";
+    break;                
+
+    case 13: //imagen error auto
+        echo "<script>";
+        echo "
+        $.toast({
+            heading: '',
+            text: '".$Texto."<img style=width:100% src=".$img.">"."',                
+            
+            icon:'error',
+            showHideTransition: 'slide'
+            
+        })
+        ";
+        echo "</script>";
+    break;                
+
+
+        default:
+           echo "<script>";
+               echo "$.toast('".$Texto."');   ";
+           echo "</script>";
+    }
+}
+
 ?>
--- _main/head.php
+++ _main/head.php
@@ -12,6 +12,13 @@ require("./_components.php");
 
 	<link rel="stylesheet" href="<?php echo $theme; ?>"/>
 	<script src="_lib/jquery-3.3.1.js"></script> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+
+
+	<link rel="stylesheet" href="_lib/jquery.toast.min.css">
+	<script type="text/javascript" src="_lib/jquery.toast.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="_lib/datatables.min.css"/> 
+	<script type="text/javascript" src="_lib/datatables.min.js"></script>
 </head>
 <body>
 	
