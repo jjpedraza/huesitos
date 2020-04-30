fbbbb855166b3ce42ad071197845eacc01fcc854
JPedraza <printepolis@gmail.com>
2020-04-30 00:25:26 -0500

    actualizacion

--- _components.php
+++ _components.php
@@ -25,6 +25,156 @@ function Test(){
 
 
 
+function DynamicTable_MySQL($sql, $IdDiv, $IdTabla, $Clase, $Tipo, $db){
+	//Tipo == 0 = Basica, 1 = ScrollVertical, 2 = Scroll Horizontal
+	//$sql = "select * from Colorines limit 20";
+	//DynamicTable_MySQL($sql, "Colorines", "Colorines_Tabla", "Colorines_ClaseCSS", 0, 0);
+
+	require("_config.php");
+	
+	if ($db == 0){
+        $r= $db0 -> query($sql);
+        $tbCont = '<div id="'.$IdDiv.'" class="'.$Clase.'">
+        <table id="'.$IdTabla.'" class="display" style="width:100%" class="tabla" style="font-size:8pt;">';
+    $tabla_titulos = ""; $cuantas_columnas = 0;
+        $r2 = $db0 -> query($sql); while($finfo = $r2->fetch_field())
+        {//OBTENER LAS COLUMNAS
+
+                /* obtener posición del puntero de campo */
+                $currentfield = $r2->current_field;       
+                $tabla_titulos=$tabla_titulos."<th style='text-transform:uppercase; font-size:9pt;'>".$finfo->name."</th>";
+                $cuantas_columnas = $cuantas_columnas + 1;        
+        }
+
+        $tbCont = $tbCont."  
+        <thead>
+        <tr>
+            ".$tabla_titulos."  
+        </tr>
+        </thead>"; //Encabezados
+        $tbCont = $tbCont."<tbody class='tabla'>";
+        $cuantas_filas=0;
+        $r = $db0 -> query($sql); while($f = $r-> fetch_row())
+        {//LISTAR COLUMNAS
+
+            $tbCont = $tbCont."<tr>";        
+            for ($i = 1; $i <= $cuantas_columnas; $i++) {      
+                $tbCont = $tbCont."<td style='font-size:10pt;'>".$f[$i-1]."</td>";       
+                }
+
+            $tbCont = $tbCont."</tr>";
+            $cuantas_filas = $cuantas_filas + 1;        
+        }
+
+        $tbCont = $tbCont."</tbody>";
+        $tbCont = $tbCont."</table></div>";
+	
+
+    }
+
+
+
+
+
+
+	if ($db == 1){
+
+        $r1= $db1 -> query($sql);
+        $tbCont = '<div id="'.$IdDiv.'" class="'.$Clase.'">
+        <table id="'.$IdTabla.'" class="display" style="width:100%" class="tabla" style="font-size:8pt;">';
+    $tabla_titulos = ""; $cuantas_columnas = 0;
+        $r1_1 = $db1 -> query($sql); while($finfo = $r1_1->fetch_field())
+        {//OBTENER LAS COLUMNAS
+
+                /* obtener posición del puntero de campo */
+                $currentfield = $r1_1->current_field;       
+                $tabla_titulos=$tabla_titulos."<th style='text-transform:uppercase; font-size:9pt;'>".$finfo->name."</th>";
+                $cuantas_columnas = $cuantas_columnas + 1;        
+        }
+
+        $tbCont = $tbCont."  
+        <thead>
+        <tr>
+            ".$tabla_titulos."  
+        </tr>
+        </thead>"; //Encabezados
+        $tbCont = $tbCont."<tbody class='tabla'>";
+        $cuantas_filas=0;
+        $r1 = $db1 -> query($sql); while($f1 = $r1-> fetch_row())
+        {//LISTAR COLUMNAS
+
+            $tbCont = $tbCont."<tr>";        
+            for ($i = 1; $i <= $cuantas_columnas; $i++) {      
+                $tbCont = $tbCont."<td style='font-size:10pt;'>".$f1[$i-1]."</td>";       
+                }
+
+            $tbCont = $tbCont."</tr>";
+            $cuantas_filas = $cuantas_filas + 1;        
+        }
+
+        $tbCont = $tbCont."</tbody>";
+        $tbCont = $tbCont."</table></div>";
+	
+
+    }
+
+
+    if ($db == 0 OR $db==1){
+	echo  $tbCont;
+		switch ($Tipo) {
+			case 1: //Scroll Vertical
+					echo '<script>
+					$(document).ready(function() {
+						$("#'.$IdTabla.'").DataTable( {
+							"scrollY":        "200px",
+							"scrollCollapse": true,
+							"paging":         false,
+							"language": {
+								"decimal": ",",
+								"thousands": "."
+							}
+						} );
+					} );
+					</script>';
+				break;
+
+			case 2: //Scroll Horizontal
+					echo '<script>
+					$(document).ready(function() {
+						$("#'.$IdTabla.'").DataTable( {
+							"scrollX": true,
+							"scrollCollapse": true,
+							"paging":         true,
+							"language": {
+								"decimal": ",",
+								"thousands": "."
+							}
+						} );
+					} );
+					</script>';
+				break;
+			
+			default:
+				echo '<script>
+				$(document).ready(function() {
+					$("#'.$IdTabla.'").DataTable( {
+						"language": {
+							"decimal": ",",
+							"thousands": "."
+						}
+					} );
+				} );
+				</script>';
+		}
+    } else {
+    	echo "Error: no se ha seleccionado una db para la Tabla Dinamica";
+    }
+
+}
+
+
+
+
 
 function Toast($Texto,$Tipo,$img){
     switch ($Tipo) {
@@ -224,6 +374,141 @@ function Toast($Texto,$Tipo,$img){
                echo "$.toast('".$Texto."');   ";
            echo "</script>";
     }
+}
+
+
+
+
+
+function GenerarGrafica($IdUser, $Token, $Gtype, $File, $Values, $LabelData, $Labels){
+require("_config.php");
+
+			switch ($Gtype) {
+				case 0:
+											//GRAFICA PIE CHART
+						require_once ("_lib/jpgraph/jpgraph.php");
+						require_once ("_lib/jpgraph/jpgraph_pie.php");
+						$data = $Values; //Array para vaalues
+						$dataLabels = $LabelData;  //Valor en las etiquetas
+						$labels = $Labels; //mas info
+				;
+						$graph = new PieGraph(2500,2500);
+						$theme_class="DefaultTheme";
+						$graph->title->Set($title);
+						$graph->subtitle->Set($subtitle);
+						$graph->SetBox(true);
+						$p1 = new PiePlot($data);
+						$p1->SetLabelType("PIE_VALUE_ABS");
+						$graph->Add($p1);
+						$p1->SetLegends($dataLabels);
+						$p1->ShowBorder();
+						$p1->SetColor('black');
+						$p1->SetLabels($labels);
+						$p1->SetGuideLines(true,false);
+						$p1->SetGuideLinesAdjust(1.1);
+						$p1->SetLabelPos(1);
+						$p1->SetLabelType(PIE_VALUE_PER);
+						$graph->legend->SetFont(FF_ARIAL,FS_NORMAL,14);
+						$graph->legend->SetPos(0.5,0.97,'center','bottom');
+						$graph->legend->SetMarkAbsSize (20);
+						// $graph->legend-> SetColumns (2);
+						$p1->value->SetFont(FF_ARIAL,FS_NORMAL,14);
+
+						$graph->title->SetFont(FF_ARIAL,FS_NORMAL,24);
+						$graph->subtitle->SetFont(FF_ARIAL,FS_NORMAL,18);
+
+						$p1->SetSliceColors(array('#F08080','#E9967A','#DC143C','#FF69B4','#C71585','#FFA07A','#FF6347','#FF8C00','#BDB76B','#D8BFD8','#EE82EE','#BA55D3','#9966CC','#9400D3','#8B008B','#4B0082','#7CFC00','#32CD32','#90EE90','#00FF7F','#2E8B57','#008000','#9ACD32','#808000','#66CDAA','#20B2AA','#008080','#00FFFF','#AFEEEE','#40E0D0','#00CED1','#4682B4','#B0E0E6','#87CEEB','#00BFFF','#6495ED','#4169E1','#0000CD','#000080','#F5DEB3','#D2B48C','#F4A460','#B8860B','#D2691E','#A0522D','#800000','#808080','#778899','#2F4F4F'));
+
+						if ($Fila == 1){
+							$graph->Stroke("tmp/graficas/".$IdUser."_".$token.".jpg");
+						} else {
+							$graph->Stroke();
+						}
+						
+					
+			
+
+				break;
+			case 1:
+				if ($Gsql<>''  AND $SQL_go == TRUE) {
+					require_once ('jpgraph/jpgraph.php');
+					require_once ('jpgraph/jpgraph_line.php');
+				
+
+					
+					$datay = array();  //Array para vaalues
+					$dataLabels = array();  
+					$labels = array();
+					// echo $Gsql;
+					switch ($BD) {
+						case "P": $r = $conexion -> query($Gsql);break;
+						case "V": $r = $Vivienda -> query($Gsql); break;		
+						default: $r = $conexion -> query($Gsql);break;	
+					}
+					// $r= $conexion -> query($Gsql);
+					// var_dump($r);
+					while($fv = $r -> fetch_array()) {//asi deben estar las consultas
+						array_push ($datay, $fv['Valor']);
+						array_push ($dataLabels, $fv['Label']);
+						array_push ($labels, "(".$fv['Valor'].") %.1f%%");
+
+						//Lineas
+						
+					
+						
+					}
+
+					
+					
+					
+					// $datay = array(0,25,12,47,27,27,0);
+					
+					// Setup the graph
+					$graph = new Graph(2500,2500);
+					$graph->SetScale("intlin",0,$aYMax=50);
+					
+					$theme_class= new UniversalTheme;
+					$graph->SetTheme($theme_class);
+					
+					$graph->SetMargin(40,40,50,40);
+					
+					$graph->title->Set($title);
+					$graph->title->SetFont(FF_ARIAL,FS_NORMAL,24);
+					$graph->title->SetMargin(50);
+					$graph->SetBox(true);
+					$graph->yaxis->HideLine(true);
+					$graph->yaxis->HideTicks(true,true);					
+					
+					$graph->SetBackgroundGradient('#FFFFFF', '#00FF7F', GRAD_HOR, BGRAD_PLOT);					
+					
+					$graph->xaxis->SetTickLabels($dataLabels);
+					$graph->xaxis->SetFont(FF_ARIAL,FS_NORMAL,14);
+					$graph->yaxis->SetFont(FF_ARIAL,FS_NORMAL,14);
+					$graph->xaxis->SetLabelMargin(20);
+					$graph->yaxis->SetLabelMargin(20);
+					
+
+					// $graph->SetAxisStyle(AXSTYLE_BOXOUT);
+					// $graph->img->SetAngle(180); 
+					
+					// Create the line
+					$p1 = new LinePlot($datay);
+					$graph->Add($p1);
+					
+					$p1->SetFillGradient('#BBBBBB','#BBBBBB');
+					$p1->SetColor('#BBBBBB');
+					
+					// Output line					
+					$graph->Stroke("tmp/graficaspdf/".$nitavu."_".$id_rep."_".$token.".jpg");
+				}
+
+			
+			
+	
+	
+	
+	
+	}
 }
 
 ?>
--- _config.php
+++ _config.php
@@ -15,4 +15,40 @@ $Empresa_domicilio = "Cd. Victoria, Tamaulipas";
 //TEMA POR DEFECTO
 $theme = "_themes/default/default.css";
 
+
+//CONEXION
+	//db0 = principila de huesitos
+	$db0_host = 'localhost';	
+	$db0_user = 'root';
+	$db0_pass = 'destino'; 
+	$db0_name = 'huesitos';
+
+	if (function_exists('mysqli_connect')) {		
+		$db0 = new mysqli($db0_host,$db0_user,$db0_pass,$db0_name);
+		$acentos = $db0->query("SET NAMES 'utf8'"); // para los acentos
+			global $db0;
+			//coneccin exitorsamente
+		}else{
+			
+			die ("Error en la conexion a la base de datos principal de huesitos");
+	}
+
+	//db1 --> Base1 de datos de tu cliente
+	$db1_isset = TRUE;
+	if ($db1_isset == TRUE){
+	$db1_host = 'localhost';	
+	$db1_user = 'root';
+	$db1_pass = 'destino'; 
+	$db1_name = 'ejemplo';
+
+	if (function_exists('mysqli_connect')) {		
+		$db1 = new mysqli($db1_host,$db1_user,$db1_pass,$db1_name);
+		$acentos1 = $db1->query("SET NAMES 'utf8'"); // para los acentos
+			global $db1;
+			//coneccin exitorsamente
+		}else{
+			
+			die ("Error en la conexion a la base de datos db1:".$db1_name);
+	}
+	}
 ?>
