73f81431f52225acbc9a9757a7bcc05ca8edf3d2
PrymeCode <printepolis@gmail.com>
2020-06-18 11:21:41 -0500

    Reestructura

---
+++ test/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie-edge">
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+
+
+    <title>Test bootstrap</title>
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="lib/popper.min.js"></script>
+<script src="lib/jquery-3.5.1.slim.min.js.min.js"></script>
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+
+    
+</body>
+</html>
